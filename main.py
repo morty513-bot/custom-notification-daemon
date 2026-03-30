bc5b7874aaa962d4b4bb52b56da6dda144238100
Ofsouzap <ofsouzap@gmail.com>
2026-03-30 18:08:34 +0100

    Refactoring

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def _run_daemon_with_renderer(renderer_name: str) -> None:
     asyncio.run(run_daemon(RendererNotificationDaemon(renderer)))
 
 
-def _renderer_option(func: Any) -> Any:
+def _renderer_option(func: click.core.F) -> click.core.F:
     return click.option(
         "--renderer",
         "renderer_name",
@@ -60,20 +60,15 @@ def _renderer_option(func: Any) -> Any:
     )(func)
 
 
-def _cli() -> None:
+@click.version_option(version=VERSION, prog_name="custom-notification-daemon")
+@click.group(
+    no_args_is_help=True,
+    context_settings={"help_option_names": ["-h", "--help"]},
+)
+def cli() -> None:
     """Custom notification daemon for Wayland/Sway."""
     pass
 
-
-cli = cast(
-    Any,
-    click.version_option(version=VERSION, prog_name="custom-notification-daemon")(
-        click.group(
-            no_args_is_help=True,
-            context_settings={"help_option_names": ["-h", "--help"]},
-        )(_cli)
-    ),
-)
 
 @cli.command("run")
 @_renderer_option
